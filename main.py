0a2e2128abee3f67f50b4488bdac97c1166505d0
joseroca99 <joseroca99@hotmail.com>
2026-02-19 21:37:15 -0500

    first webhook

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import PlainTextResponse
+import os
+from datetime import datetime
+import json
 
 app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
-def home():
-    return "<h1>FastAPI running on Render 🚀</h1>"
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+
+@app.get("/", response_class=PlainTextResponse)
+async def verify_webhook(
+    request: Request,
+):
+    params = request.query_params
+
+    mode = params.get("hub.mode")
+    challenge = params.get("hub.challenge")
+    token = params.get("hub.verify_token")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        print("WEBHOOK VERIFIED")
+        return challenge
+
+    raise HTTPException(status_code=403)
+
+
+@app.post("/")
+async def receive_webhook(request: Request):
+    body = await request.json()
+
+    timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"\n\nWebhook received {timestamp}\n")
+    print(json.dumps(body, indent=2))
+
+    return {"status": "ok"}
